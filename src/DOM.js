9c7bbf27738a7f1867dd8c55823e74067b71af4c
Aze <azrielle101@gmail.com>
2020-07-01 22:41:19 +0800

    Created hideProjectForm and createProjectElem

--- src/DOM.js
+++ src/DOM.js
@@ -5,7 +5,10 @@ const ManipulateDOM = (() => {
     const close = document.querySelector("#close");
 
     const inputProject = document.querySelector('#addProjectTitle');
-    const addProjectButton = document.querySelector('.addProject')
+    const addProjectButton = document.querySelector('.addProject');
+
+    let projectList = document.querySelector(".projectList");
+
 
     const burgerToggle = ()=> {
       burgerNav.addEventListener('click', () =>{
@@ -23,7 +26,24 @@ const ManipulateDOM = (() => {
         })
     };
 
-    return {burgerToggle, showProjectForm};
+    const hideProjectForm = () =>{
+        addProjectButton.style.display = "block";
+        inputProject.style.display = "none";
+    }
+
+    const createProjectElem = (array) =>{
+        let name = document.createElement("p");
+        name.textContent = array;
+        projectList.appendChild(name);
+    }
+
+    
+    return {
+        burgerToggle, 
+        showProjectForm, 
+        createProjectElem, 
+        hideProjectForm
+    };
   
   })();
   
